7cfa1d330eb418f59558ede9f92ab9d8435e1f34
Marek Chałabis <marek.chalabis@tylko.com>
2022-01-19 12:17:44 +0100

    Fix validators for account split

--- controller_server/backend/src/api/api_v1/endpoints/account_split.py
+++ controller_server/backend/src/api/api_v1/endpoints/account_split.py
@@ -1,15 +1,14 @@
 from fastapi import APIRouter, status
-from src.custom_types import AccountsSplitData
 from src.redis import r
-from src.validators import validate_accounts_split
+
+from controller_server.backend.src.schemas import AccountsSplits
 
 router = APIRouter()
 
 
 @router.post("/", status_code=status.HTTP_202_ACCEPTED)
 async def splits_accounts(
-        accounts_split_data: AccountsSplitData,
+        accounts_split_data: AccountsSplits,
 ) -> None:
     """Saves Accounts trade split."""
-    validate_accounts_split(accounts_split_data=accounts_split_data)
-    r.hmset('accounts_split_data', accounts_split_data)
+    r.hmset('accounts_split_data', accounts_split_data.json())
