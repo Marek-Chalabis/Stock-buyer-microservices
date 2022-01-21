2e2ca5338b97503055ada525fc6f531abc0c3717
Marek Chałabis <marek.chalabis@tylko.com>
2022-01-21 09:15:00 +0100

    Add random creation of fills

---
+++ fill_server/backend/src/fills/constants.py
@@ -0,0 +1,16 @@
+STOCK_TICKERS = [
+    '1AB',
+    '2CD',
+    '3EF',
+    '4GH',
+    '5IJ',
+    '6KL',
+    '7MN',
+    '8OP',
+    '9RS',
+    '10T',
+]
+MIN_TRADE_TICKER_QUANTITY = 1
+MAX_TRADE_TICKER_QUANTITY = 100
+MIN_TRADE_TICKER_PRICE = 50
+MAX_TRADE_TICKER_PRICE = 5000
---
+++ fill_server/backend/src/fills/fills.py
@@ -0,0 +1,32 @@
+import random
+
+from typing import (
+    Dict,
+    Union,
+)
+
+from src.fills.constants import (
+    MAX_TRADE_TICKER_PRICE,
+    MAX_TRADE_TICKER_QUANTITY,
+    MIN_TRADE_TICKER_PRICE,
+    MIN_TRADE_TICKER_QUANTITY,
+    STOCK_TICKERS,
+)
+
+TRADE_FILL = Dict[str, Union[str, float]]
+
+
+def get_random_fill() -> TRADE_FILL:
+    """Radnom trade fill with random quantity and price."""
+    random_trade_ticker = random.choice(STOCK_TICKERS)
+    random_quantity = random.randint(
+        MIN_TRADE_TICKER_QUANTITY, MAX_TRADE_TICKER_QUANTITY
+    )
+    random_price = round(
+        random.uniform(MIN_TRADE_TICKER_PRICE, MAX_TRADE_TICKER_PRICE), 2
+    )
+    return {
+        'stock_ticker': random_trade_ticker,
+        'price': random_price,
+        'quantity': random_quantity,
+    }
