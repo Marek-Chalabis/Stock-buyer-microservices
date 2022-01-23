d2d16ca88a3e566336742e8e7b3bd7ddc7a842fa
Marek Chałabis <marek.chalabis@tylko.com>
2022-01-23 21:19:42 +0100

    add test for tasks

--- controller_server/backend/src/orders/tasks.py
+++ controller_server/backend/src/orders/tasks.py
@@ -5,7 +5,7 @@ from src.core.config import settings
 from src.orders.stock_order import StockPurchaseOrder
 
 
-async def send_stock_purchase_order_to_reporter(trade_fill: TradeFill) -> None:
+def send_stock_purchase_order_to_reporter(trade_fill: TradeFill) -> None:
     endpoint = settings.reporter_server_client['stocks-purchase-orders']
     stock_purchase_order = StockPurchaseOrder(
         trade_fill=trade_fill,
