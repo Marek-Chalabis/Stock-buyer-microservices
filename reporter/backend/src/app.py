25d11bf559aa218ea0bba595571f6569b5723340
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-01 20:22:46 +0100

    Add factory for app

---
+++ reporter/backend/src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+from config import FlaskConfig
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
+
+
+def create_app(config: type[FlaskConfig] = FlaskConfig) -> Flask:
+    app = Flask(import_name=__name__)
+    app.config.from_object(obj=config)
+    db.init_app(app=app)
+    bcrypt.init_app(app=app)
+    login_manager.init_app(app=app)
+
+    from users import users
+    app.register_blueprint(users)
+
+    return app
