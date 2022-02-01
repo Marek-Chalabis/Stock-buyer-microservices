54c36705cdc1dc7ee898e08f6b9f7c3c64b488e8
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-01 20:20:42 +0100

    Add model for user

---
+++ reporter/backend/src/users/models.py
@@ -0,0 +1,34 @@
+from flask_login import UserMixin
+from sqlalchemy.dialects.postgresql import MONEY
+
+from app import (
+    bcrypt,
+    db,
+)
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    email = db.Column(db.String(length=60), nullable=False, unique=True)
+    money = db.Column(
+        MONEY,
+        nullable=False,
+        default=0,
+    )
+    automatic_trade = db.Column(db.Boolean(), default=False, nullable=False)
+    # trade_operations = db.relationship('TradeOperation', backref='user', lazy=True)
+
+    @property
+    def password(self) -> str:
+        return self.password
+
+    @password.setter
+    def password(self, plain_password) -> None:
+        self.password_hash = bcrypt.generate_password_hash(plain_password).decode(
+            'utf-8'
+        )
+
+    def verify_password(self, password) -> bool:
+        return bcrypt.check_password_hash(self.password_hash, password)
