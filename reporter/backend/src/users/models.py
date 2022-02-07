5f8772c559ff431e04272776dc09529c453cbe29
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-07 20:29:02 +0100

    Add UserProfile model

--- reporter/backend/src/users/models.py
+++ reporter/backend/src/users/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from flask_login import UserMixin
 from sqlalchemy.dialects.postgresql import MONEY
 
@@ -8,16 +10,12 @@ from app import (
 
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     password_hash = db.Column(db.String(length=60), nullable=False)
     email = db.Column(db.String(length=60), nullable=False, unique=True)
-    money = db.Column(
-        MONEY,
-        nullable=False,
-        default=0,
-    )
-    automatic_trade = db.Column(db.Boolean(), default=False, nullable=False)
+
+    user_profile = db.relationship('UserProfile', backref='user', uselist=False)
     stock_trades = db.relationship('StockTrade', backref='user', lazy='dynamic')
 
     @property
@@ -32,3 +30,25 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password) -> bool:
         return bcrypt.check_password_hash(self.password_hash, password)
+
+    @classmethod
+    def create(cls, username: str, email: str, password: str) -> 'User':
+        user_to_create = cls(username=username, email=email, password=password)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return user_to_create
+
+
+class UserProfile(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    money = db.Column(
+        MONEY,
+        nullable=False,
+        default=0,
+    )
+    trade_bot = db.Column(db.Boolean, default=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    @property
+    def money_in_decimal(self) -> Decimal:
+        return Decimal(self.money[1:])
