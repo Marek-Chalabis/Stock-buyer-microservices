ab218c8ba84f18ee3242f400aadfa1d4635c0901
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-07 09:42:08 +0100

    Add more logic to JS

--- reporter/backend/src/static/js/main.js
+++ reporter/backend/src/static/js/main.js
@@ -9,18 +9,37 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    function setColorForPriceDifference() {
-        const priceDifferences = document.querySelectorAll('.price-difference')
-        const bootstrapStyle = getBootstrapStyle();
-        priceDifferences.forEach((priceDifferance) => {
-            const difference = priceDifferance.textContent.split('%')[0]
-            if (difference) {
-                priceDifferance.style.color = difference > 0
-                    ? bootstrapStyle.color.success
-                    : bootstrapStyle.color.danger
-            }
-        })
+    function adjustDiffNode(node) {
+        const bootstrapStyle = getBootstrapStyle()
+        if (['None', '0.00', '$0.00'].includes(node.textContent)) {
+            node.textContent = ''
+        } else if (node.textContent.startsWith('-')) {
+            node.style.color = bootstrapStyle.color.danger
+        } else {
+            node.textContent = '+' + node.textContent
+            node.style.color = bootstrapStyle.color.success
+        }
     }
 
-    setColorForPriceDifference()
+    function setPlainDifference() {
+        const differences = document.querySelectorAll('.plain-difference')
+        differences.forEach((difference) => {
+                adjustDiffNode(difference)
+            }
+        )
+    }
+
+    function setPercentDifference() {
+        const differences = document.querySelectorAll('.percent-difference')
+        differences.forEach((difference) => {
+                adjustDiffNode(difference)
+                if (difference.textContent) {
+                    difference.textContent += '%'
+                }
+            }
+        )
+    }
+
+    setPlainDifference()
+    setPercentDifference()
 })
