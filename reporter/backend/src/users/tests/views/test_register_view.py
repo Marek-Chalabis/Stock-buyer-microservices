df4cf56280d0104a90404144eef6b5c797b980c2
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-08 09:00:07 +0100

    Simplify register view

--- reporter/backend/src/users/tests/views/test_register_view.py
+++ reporter/backend/src/users/tests/views/test_register_view.py
@@ -1,8 +1,5 @@
-from unittest.mock import call
-
 import pytest
 
-from users.models import User
 from users.view import RegisterView
 
 
@@ -10,85 +7,66 @@ class TestRegisterView:
     def test_view_methods(self):
         assert RegisterView.methods == ['GET', 'POST']
 
-    @pytest.mark.parametrize(
-        ('tested_form_valid', 'tested_method_call'),
-        [
-            (False, '_handle_incorrect_form_validation'),
-            (True, '_handle_correct_form_validation'),
-        ],
-    )
-    def test_dispatch_request(self, mocker, tested_form_valid, tested_method_call):
-        mocker_method_call = mocker.patch(
-            f'users.view.RegisterView.{tested_method_call}'
-        )
-        mocker_register_form = mocker.patch('users.view.RegisterForm')
-        mocker_register_form.return_value = mocker.Mock(
-            validate_on_submit=mocker.Mock(return_value=tested_form_valid),
-        )
-        RegisterView().dispatch_request()
-        mocker_method_call.assert_called_once()
-
-    def test_handle_incorrect_form_validation_errors(self, mocker):
-        mocker_flash = mocker.patch('users.view.flash')
-        mocker_register_form = mocker.patch('users.view.RegisterForm')
-        mocker_register_form.return_value = mocker.Mock(
-            errors={
-                'test_error1': ['test_error1'],
-                'test_error2': ['test_error2'],
-            },
-        )
-        RegisterView()._handle_incorrect_form_validation()
-        mocker_flash.assert_has_calls(
-            [
-                call('test_error1', category='danger'),
-                call('test_error2', category='danger'),
-            ],
-        )
-
-    def test_handle_incorrect_form_validation_template(self, mocker):
-        mocker_register_form = mocker.patch('users.view.RegisterForm')
-        mocker_form = mocker.Mock(errors=None)
-        mocker_register_form.return_value = mocker_form
-        mocker_render_template = mocker.patch('users.view.render_template')
-        RegisterView()._handle_incorrect_form_validation()
-        mocker_render_template.assert_called_once_with(
-            template_name_or_list='register.html',
-            form=mocker_form,
-        )
-
-    def test_handle_correct_form_validation_user_create(self, user, mocker):
-        mocker.patch('users.view.RegisterView._get_user_from_form', return_value=user)
-        RegisterView()._handle_correct_form_validation()
-        assert User.query.get(1) == user
-
-    def test_handle_correct_form_validation_login(self, user, mocker):
-        mocker.patch('users.view.RegisterView._get_user_from_form', return_value=user)
-        mocker_login_user = mocker.patch('users.view.login_user')
-        RegisterView()._handle_correct_form_validation()
-        mocker_login_user.assert_called_once_with(user=user)
-
-    def test_handle_correct_form_validation_flash(self, user, mocker):
-        mocker.patch('users.view.RegisterView._get_user_from_form', return_value=user)
-        mocker_flash = mocker.patch('users.view.flash')
-        RegisterView()._handle_correct_form_validation()
-        mocker_flash.assert_called_once()
-
-    def test_handle_correct_form_validation_redirect(self, user, mocker):
-        mocker.patch('users.view.RegisterView._get_user_from_form', return_value=user)
-        mocker_redirect = mocker.patch('users.view.redirect')
-        mocker_url_for = mocker.patch('users.view.url_for')
-        RegisterView()._handle_correct_form_validation()
-        mocker_url_for.assert_called_once_with(endpoint='users.home_page')
-        mocker_redirect.assert_called_once_with(location=mocker_url_for())
-
-    def test_get_user_from_form(self, user, mocker):
+    @pytest.fixture
+    def register_form(self, mocker):
         mocker_register_form = mocker.patch('users.view.RegisterForm')
         mocker_form = mocker.Mock(
+            validate_on_submit=mocker.Mock(return_value=False),
             username=mocker.Mock(data='test_username'),
             email=mocker.Mock(data='test_email'),
             password=mocker.Mock(data='test_password'),
         )
         mocker_register_form.return_value = mocker_form
-        tested_user = RegisterView()._get_user_from_form()
-        assert tested_user.username == 'test_username'
-        assert tested_user.email == 'test_email'
+        return mocker_register_form
+
+    def test_dispatch_request_validate_correct(self, mocker):
+        mocker_handle_correct_register_form = mocker.patch(
+            'users.view.RegisterView._handle_correct_register_form',
+        )
+        mocker_register_form = mocker.patch('users.view.RegisterForm')
+        mocker_register_form.return_value = mocker.Mock(
+            validate_on_submit=mocker.Mock(return_value=True)
+        )
+        RegisterView().dispatch_request()
+        mocker_handle_correct_register_form.assert_called_once_with()
+
+    def test_dispatch_request_validate_incorrect(self, mocker, register_form):
+        mocker.patch('users.view.flash_errors_from_form')
+        mocker_render_template = mocker.patch('users.view.render_template')
+        RegisterView().dispatch_request()
+        mocker_render_template.assert_called_once_with(
+            template_name_or_list='register.html',
+            form=register_form.return_value,
+        )
+
+    def test_dispatch_request_flash_errors(self, mocker, register_form):
+        mocker_flash_errors_from_form = mocker.patch(
+            'users.view.flash_errors_from_form',
+        )
+        RegisterView().dispatch_request()
+        mocker_flash_errors_from_form.assert_called_once_with(
+            form=register_form.return_value,
+        )
+
+    def test_handle_correct_register_form_user_create(self, mocker, register_form):
+        mocker_create = mocker.patch('users.view.User.create')
+        RegisterView()._handle_correct_register_form()
+        mocker_create.assert_called_once_with(
+            username=register_form.return_value.data,
+            email=register_form.return_value.data,
+            password='test_password',
+        )
+
+    def test_handle_correct_register_form_redirect(self, mocker, register_form):
+        mocker.patch('users.view.User.create')
+        mocker_flash = mocker.patch('users.view.flash')
+        RegisterView()._handle_correct_register_form()
+        mocker_flash.assert_called_once()
+
+    def test_handle_correct_register_form_flash(self, mocker, register_form):
+        mocker.patch('users.view.User.create')
+        mocker_redirect = mocker.patch('users.view.redirect')
+        mocker_url_for = mocker.patch('users.view.url_for')
+        RegisterView()._handle_correct_register_form()
+        mocker_url_for.assert_called_once_with(endpoint='trades.trades_page')
+        mocker_redirect.assert_called_once_with(location=mocker_url_for())
--- reporter/backend/src/users/view.py
+++ reporter/backend/src/users/view.py
@@ -10,50 +10,41 @@ from flask.typing import ResponseReturnValue
 from flask.views import View
 from flask_login import login_user
 
-from app import db
 from users.forms import (
     LoginForm,
     RegisterForm,
 )
 from users.models import User
+from utils.view_utils import flash_errors_from_form
 
 
 class RegisterView(View):
     methods = ['GET', 'POST']
 
     def __init__(self) -> None:
-        self._form = RegisterForm()
+        self._register_form = RegisterForm()
 
     def dispatch_request(self) -> ResponseReturnValue:
-        if self._form.validate_on_submit():
-            return self._handle_correct_form_validation()
-        return self._handle_incorrect_form_validation()
-
-    def _get_user_from_form(self) -> User:
-        return User(
-            username=self._form.username.data,
-            email=self._form.email.data,
-            password=self._form.password.data,
+        if self._register_form.validate_on_submit():
+            return self._handle_correct_register_form()
+        else:
+            flash_errors_from_form(form=self._register_form)
+        return render_template(
+            template_name_or_list='register.html', form=self._register_form
         )
 
-    def _handle_correct_form_validation(self) -> ResponseReturnValue:
-        user_to_create = self._get_user_from_form()
-        db.session.add(user_to_create)
-        db.session.commit()
+    def _handle_correct_register_form(self) -> ResponseReturnValue:
+        user_to_create = User.create(
+            username=self._register_form.data,
+            email=self._register_form.data,
+            password=self._register_form.password.data,
+        )
         login_user(user=user_to_create)
-        # TODO add link to add money and change redirect template for trades
         flash(
             f'Account {user_to_create.username} created successfully',
             category='success',
         )
-
-        return redirect(location=url_for(endpoint='users.home_page'))
-
-    def _handle_incorrect_form_validation(self) -> ResponseReturnValue:
-        if self._form.errors:
-            for err_msg in self._form.errors.values():
-                flash(*err_msg, category='danger')
-        return render_template(template_name_or_list='register.html', form=self._form)
+        return redirect(location=url_for(endpoint='trades.trades_page'))
 
 
 class LoginView(View):
