9199d517afd730284d6d311c19867744c3243741
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-08 14:11:19 +0100

    Remove redundant validation from login form tests

--- reporter/backend/src/users/tests/test_forms.py
+++ reporter/backend/src/users/tests/test_forms.py
@@ -1,10 +1,7 @@
 import pytest
 import wtforms
 
-from users.forms import (
-    LoginForm,
-    RegisterForm,
-)
+from users.forms import RegisterForm
 
 
 class TestRegisterForm:
@@ -25,16 +22,3 @@ class TestRegisterForm:
         mock_email = mocker.Mock(data='test_email')
         with pytest.raises(wtforms.validators.ValidationError):
             RegisterForm().validate_email(mock_email)
-
-
-class TestLoginForm:
-    def test_validate_username_no_user(self, mocker):
-        mock_username = mocker.Mock(data='username_that_does_not_exists')
-        with pytest.raises(wtforms.validators.ValidationError):
-            LoginForm().validate_username(mock_username)
-
-    def test_validate_password_wrong_password(self, mocker, user_in_db):
-        mocker.patch('users.forms.User.get_user_by_username', return_value=user_in_db)
-        mock_password = mocker.Mock(data='wrong_password')
-        with pytest.raises(wtforms.validators.ValidationError):
-            LoginForm().validate_password(mock_password)
