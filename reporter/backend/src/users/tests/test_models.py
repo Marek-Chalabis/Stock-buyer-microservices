9b7d1aab5c8015cc588bc3502e40b0133de0cd10
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-08 07:21:49 +0100

    Add tests for profile

--- reporter/backend/src/users/tests/test_models.py
+++ reporter/backend/src/users/tests/test_models.py
@@ -1,4 +1,9 @@
+from decimal import Decimal
+
 import pytest
+
+from app import db
+from users.models import User
 
 
 class TestUsers:
@@ -14,3 +19,24 @@ class TestUsers:
     )
     def test_verify_password(self, user_in_db, tested_password, expected_result):
         assert user_in_db.verify_password(tested_password) == expected_result
+
+    def test_create(self):
+        tested_username = 'test_username'
+        tested_email = 'test_email'
+        tested_user_from_create = User.create(
+            username=tested_username,
+            email=tested_email,
+            password='test_password',
+        )
+        tested_user_from_db = db.session.query(User).first()
+        assert tested_user_from_db.username == tested_user_from_create.username
+        assert tested_user_from_db.email == tested_user_from_create.email
+
+
+class TestUserProfile:
+    def test_user_profile_relationship(self, user_in_db):
+        assert user_in_db.user_profile
+        assert user_in_db.user_profile.user == user_in_db
+
+    def test_money_in_decimal(self, user_in_db):
+        assert user_in_db.user_profile.money_in_decimal == Decimal(0)
