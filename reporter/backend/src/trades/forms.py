71850424968a89e6d912719a4178649051c53787
Marek Chałabis <marek.chalabis@tylko.com>
2022-02-14 19:55:49 +0100

    Add form for selling trades

--- reporter/backend/src/trades/forms.py
+++ reporter/backend/src/trades/forms.py
@@ -1,6 +1,11 @@
 import wtforms
 
 from flask import request
+from flask_login import (
+    current_user,
+    login_required,
+    login_user,
+)
 from flask_wtf import FlaskForm
 
 from trades.models import Stock
@@ -25,4 +30,28 @@ class BuyTradesForm(FlaskForm):
             raise wtforms.ValidationError(
                 f'You are trying to buy more stocks("{stock_symbol}") '
                 + f'then are available({available_quantity})',
+            )
+
+
+class SellTradesForm(FlaskForm):
+    amount = wtforms.DecimalField(
+        label='Amount:',
+        validators=[
+            wtforms.validators.DataRequired(),
+            wtforms.validators.NumberRange(min=1),
+        ],
+    )
+    submit_sell_trades = wtforms.SubmitField(label='Sell trades')
+
+    def validate_amount(self, amount_to_validate: wtforms.DecimalField) -> None:
+        stock_symbol = request.form.get('sold_stock')
+        available_quantity = next(
+            stock
+            for stock in current_user.quantity_of_acquired_trades
+            if stock.symbol == stock_symbol.symbol
+        ).currently_acquired
+        if amount_to_validate.data > available_quantity:
+            raise wtforms.ValidationError(
+                f'You are trying to sell more stocks("{stock_symbol}") '
+                + f'then you have({available_quantity})',
             )
